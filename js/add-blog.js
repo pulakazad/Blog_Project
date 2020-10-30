dbfee5ca56630b4b9a7734c5418f67e39cc53cef
pulakazad <pulakazad@gatech.edu>
2020-10-30 10:58:12 -0400

    added blogs page and addBlog() form

---
+++ js/add-blog.js
@@ -0,0 +1,15 @@
+function displayBlogForm() {
+    document.getElementById('addBlogForm').style.display = "block"
+}
+
+function hideBlogForm() {
+    document.getElementById('addBlogForm').style.display = "none";
+}
+
+function newBlog() {
+
+    
+
+
+    hideBlogForm();
+}
